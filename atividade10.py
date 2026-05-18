653318c61922a65f3c64dc300fa7b7b14d9ad2bd
Bernardo-Carvalho-Lima <limabernardo015@gmail.com>
2026-05-18 14:47:29 -0300

    2-UC

---
+++ atividade10.py
@@ -0,0 +1,22 @@
+import pandas as pd
+
+dados = {
+    'Fruta': ['Maçã', 'Banana', 'Laranja', 'Morango', 'Uva'],
+    'Quantidade': [10, 15, 8, 20, 12],
+    'Preco': [2.50, 1.20, 3.00, 5.50, 4.00]}
+
+df = pd.DataFrame(dados)
+
+print("--- Meu Primeiro DataFrame ---")
+print(df)
+
+#atividade 2
+import pandas as pd
+
+df_produtos = pd.read_csv("produtos.csv")
+
+print("--- Dados Carregados do Arquivo CSV ---")
+print(df_produtos)
+
+print("\n--- Resumo das primeiras linhas ---")
+print(df_produtos.head(2))
